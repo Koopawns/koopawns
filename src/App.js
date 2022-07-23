59756b7e369557b55d738a95462d7cfda1f0c072
koopawnns <koopawns2k@gmail.com>
2022-07-23 03:42:43 +0300

    SecondCommit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App()
+{
+    return
+    (
+      <div className="App">
+        <h1>Hello World</h1>
+        <p>Khan</p>
+      </div>
+    );
+}
+
+export default App;
